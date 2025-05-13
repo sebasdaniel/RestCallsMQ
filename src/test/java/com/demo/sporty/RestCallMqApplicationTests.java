07948deb31ced1b67606e2f797deb4f4dc0876fc
PlayerOne <pe.sebas@gmail.com>
2025-05-13 17:37:50 -0500

    Added spring boot structure

---
+++ src/test/java/com/demo/sporty/RestCallMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.sporty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCallMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
